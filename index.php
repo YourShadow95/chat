b73f08c86c58f9a4c2908168f5188761e5b2da7f
abdrakhman <gagnger95@gmail.com>
2020-02-22 17:44:34 +0300

    Add css and update index.html

--- index.php
+++ index.php
@@ -9,8 +9,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My chat</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div id="wrapper">
+        <h1>Welcome to my chat</h1>
+        <div class="chat_wrapper">
+            <div id="chat">
+            </div>
+            <form method="post">
+                <textarea name="message" cols="30" rows="10" class="textarea"></textarea>
+
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
